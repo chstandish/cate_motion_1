8400e431210e930683298647c6ac2d6f8771de2b
chstandish <catherine.h.standish@gmail.com>
2022-10-18 11:29:20 -0400

    started animations

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,5 +3,11 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+function mythAnimation(){
+    var tl = gsap.timeline();
+    tl.to("#hekate", {duration:1, x:500})
+    return tl;
+}
 
-
+var mainTimeline = gsap.timeline();
+mainTimeline.add(mythAnimation());
