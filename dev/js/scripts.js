04f017a31250018979b6b61f5496fb146d639c6c
chstandish <catherine.h.standish@gmail.com>
2022-10-18 12:04:09 -0400

    animation progress

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,9 +5,17 @@ gsap.registerPlugin(ScrollTrigger);
 
 function mythAnimation(){
     var tl = gsap.timeline();
-    tl.from("#hekate", {duration:1, x:-500})
+    tl.from("#hekate", {duration:1.5, alpha:0, x: -500})
+    .from("#goddess", {duration:1.5, alpha:0, x: 500});
     return tl;
 }
 
+function imageAnimation(){
+    var tl = gsap.timeline();
+    tl.from("#hero-2", {duration:3, clipPath:"inset(0 50%)"})
+}
+
 var mainTimeline = gsap.timeline();
-mainTimeline.add(mythAnimation());
+mainTimeline.add(mythAnimation())
+.add(imageAnimation());
+
