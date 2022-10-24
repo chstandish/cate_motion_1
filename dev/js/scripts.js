e8e2a4ae53388ff60d52c17398237e13f2980af1
chstandish <catherine.h.standish@gmail.com>
2022-10-24 15:53:09 -0400

    homework week 4 finished

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,6 +3,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+//timeline animations
 function mythAnimation(){
     var tl = gsap.timeline({scrollTrigger:{trigger: "#hekate", markers:false, scrub: true, end: "bottom 40%", start: "top 90%"}});
     tl.from("#myth-title", {duration: 1.5, alpha: 0, y: -30})
@@ -54,21 +55,100 @@ mainTimeline.add(mythAnimation())
 .add(galleryAnimation())
 .add(writersAnimation());
 
-// let galleryItemOne = document.querySelector("#gallery-1")
+//gallery button animations - these are really repetitive but i'm not sure how to fit them all together
+var galleryOneAnimation = gsap.timeline({paused:false});
+galleryOneAnimation.to("#gallery-1", {duration: 0.25, scale: 1.07});
 
-// var buttonSize=1;
+let galleryItemOne = document.querySelector("#gallery-1")
 
-// matchMedia.add("(min-width: 800px)", () => {
-//     buttonSize = 2;
-// });
+galleryItemOne.addEventListener("mouseover", function(){
+    galleryOneAnimation.play();
+});
 
-// matchMedia.add("(max-width: 799px)", () => {
-//     buttonSize = 1.25;
-// });
+galleryItemOne.addEventListener("mouseout", function(){
+    galleryOneAnimation.reverse();
+});
 
-// var galleryOne = gsap.timeline({paused:true});
-// galleryOne.to("#gallery-1", {duration: 0.25, scale: buttonSize});
 
-// galleryItemOne.addEventListener("mouseover", function(){
-//     galleryOne.play();
-// });
+var galleryTwoAnimation = gsap.timeline({paused:false});
+galleryTwoAnimation.to("#gallery-2", {duration: 0.25, scale: 1.07});
+
+let galleryItemTwo = document.querySelector("#gallery-2")
+
+galleryItemTwo.addEventListener("mouseover", function(){
+    galleryTwoAnimation.play();
+});
+
+galleryItemTwo.addEventListener("mouseout", function(){
+    galleryTwoAnimation.reverse();
+});
+
+
+var galleryThreeAnimation = gsap.timeline({paused:false});
+galleryThreeAnimation.to("#gallery-3", {duration: 0.25, scale: 1.05});
+
+let galleryItemThree = document.querySelector("#gallery-3")
+
+galleryItemThree.addEventListener("mouseover", function(){
+    galleryThreeAnimation.play();
+});
+
+galleryItemThree.addEventListener("mouseout", function(){
+    galleryThreeAnimation.reverse();
+});
+
+
+var galleryFourAnimation = gsap.timeline({paused:false});
+galleryFourAnimation.to("#gallery-4", {duration: 0.25, scale: 1.05});
+
+let galleryItemFour = document.querySelector("#gallery-4")
+
+galleryItemFour.addEventListener("mouseover", function(){
+    galleryFourAnimation.play();
+});
+
+galleryItemFour.addEventListener("mouseout", function(){
+    galleryFourAnimation.reverse();
+});
+
+
+var galleryFiveAnimation = gsap.timeline({paused:false});
+galleryFiveAnimation.to("#gallery-5", {duration: 0.25, scale: 1.07});
+
+let galleryItemFive = document.querySelector("#gallery-5")
+
+galleryItemFive.addEventListener("mouseover", function(){
+    galleryFiveAnimation.play();
+});
+
+galleryItemFive.addEventListener("mouseout", function(){
+    galleryFiveAnimation.reverse();
+});
+
+
+var gallerySixAnimation = gsap.timeline({paused:false});
+gallerySixAnimation.to("#gallery-6", {duration: 0.25, scale: 1.07});
+
+let galleryItemSix = document.querySelector("#gallery-6")
+
+galleryItemSix.addEventListener("mouseover", function(){
+    gallerySixAnimation.play();
+});
+
+galleryItemSix.addEventListener("mouseout", function(){
+     gallerySixAnimation.reverse();
+});
+
+
+var gallerySevenAnimation = gsap.timeline({paused:false});
+gallerySevenAnimation.to("#gallery-7", {duration: 0.25, scale: 1.07});
+
+let galleryItemSeven = document.querySelector("#gallery-7")
+
+galleryItemSeven.addEventListener("mouseover", function(){
+    gallerySevenAnimation.play();
+});
+
+galleryItemSeven.addEventListener("mouseout", function(){
+    gallerySevenAnimation.reverse();
+});
