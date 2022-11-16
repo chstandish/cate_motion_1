e8e5840c97ef97efe30ccbe07db8b8916553b945
chstandish <catherine.h.standish@gmail.com>
2022-11-16 11:16:40 -0500

    import gsap

---
+++ dev/js/scripts.js
@@ -0,0 +1,8 @@
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
+
