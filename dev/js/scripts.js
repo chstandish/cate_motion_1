da5243d8203389333b10e091f7a0134e328d2f8e
chstandish <catherine.h.standish@gmail.com>
2022-11-17 22:45:07 -0500

    progress

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,3 +6,16 @@ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
 
+function wAnimation(){
+  var tl = gsap.timeline();
+  tl.from("#middle", {duration:3, morphSVG:"#middleStart"});
+  tl.fromTo("#left",{fill:"rgba(0,0,0,0", drawSVG: "0% 0%"}, {duration: 2, drawSVG:"0% 100%"}, "<50%");
+  tl.fromTo("#right",{fill:"rgba(0,0,0,0", drawSVG: "54% 54%"}, {duration: 2, drawSVG:"54% -54%"}, "<0%");
+  return tl;
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(wAnimation());
+
+
+GSDevTools.create();
